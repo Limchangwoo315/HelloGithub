918301a19d8582bd419b8b4827f963c9800d7f01
idaehyeon <leedaehyun11@jbnu.ac.kr>
2024-09-25 14:58:11 +0900

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;  // Scanner 클래스를 사용하기 위해 임포트
+
 public class Main {
     public static void main(String[] args) {
+        // Scanner 객체 생성
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Hello and welcome!");
+        // 환영 메시지 출력
+        System.out.print("Hello and welcome! ");
 
+        // 정수 입력받기
+        int a = scanner.nextInt();
+
+        // 입력받은 정수 출력
+        System.out.print(a);
+
+        // Scanner 닫기
+        scanner.close();
     }
 }
