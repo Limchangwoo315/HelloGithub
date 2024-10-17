b1d9d95c35aab026263e329a072d0035814175a5
exlle <exller@naver.com>
2024-10-18 00:05:25 +0900

    총기 반동 추가완료

--- shoottheduck/src/main/java/kr/jbnu/se/std/Framework.java
+++ shoottheduck/src/main/java/kr/jbnu/se/std/Framework.java
@@ -12,12 +12,12 @@ import javax.imageio.ImageIO;
 
 /**
  * kr.jbnu.se.std.Framework that controls the game (kr.jbnu.se.std.Game.java) that created it, update it and draw it on the screen.
- * 
+ *
  * @author www.gametutorial.net
  */
 
 public class Framework extends Canvas {
-    
+
     /**
      * Width of the frame.
      */
@@ -32,13 +32,13 @@ public class Framework extends Canvas {
      * 1 second = 1 000 000 000 nanoseconds
      */
     public static final long secInNanosec = 1000000000L;
-    
+
     /**
      * Time of one millisecond in nanoseconds.
      * 1 millisecond = 1 000 000 nanoseconds
      */
     public static final long milisecInNanosec = 1000000L;
-    
+
     /**
      * FPS - Frames per second
      * How many times per second the game should update?
@@ -48,7 +48,7 @@ public class Framework extends Canvas {
      * Pause between updates. It is in nanoseconds.
      */
     private final long GAME_UPDATE_PERIOD = secInNanosec / GAME_FPS;
-    
+
     /**
      * Possible states of the game
      */
@@ -57,14 +57,14 @@ public class Framework extends Canvas {
      * Current state of the game
      */
     public static GameState gameState;
-    
+
     /**
      * Elapsed game time in nanoseconds.
      */
     private long gameTime;
     // It is used for calculating elapsed time.
     private long lastTime;
-    
+
     // The actual game
     private Game game;
 
@@ -72,15 +72,17 @@ public class Framework extends Canvas {
     /**
      * Image for menu.
      */
-    private BufferedImage shootTheDuckMenuImg;    
-    
-    
+    private BufferedImage shootTheDuckMenuImg;
+
+    private Sight sight;
+
     public Framework ()
     {
         super();
         player = new Player();
+        sight = new Sight(); // Sight 객체 생성
         gameState = GameState.VISUALIZING;
-        
+
         //We start game in new thread.
         Thread gameThread = new Thread() {
             @Override
@@ -90,9 +92,9 @@ public class Framework extends Canvas {
         };
         gameThread.start();
     }
-    
-    
-   /**
+
+
+    /**
      * Set variables and objects.
      * This method is intended to set the variables and objects for this class, variables and objects for the actual game can be set in kr.jbnu.se.std.Game.java.
      */
@@ -100,7 +102,7 @@ public class Framework extends Canvas {
     {
 
     }
-    
+
     /**
      * Load files - images, sounds, ...
      * This method is intended to load files for this class, files for the actual game can be loaded in kr.jbnu.se.std.Game.java.
@@ -116,7 +118,7 @@ public class Framework extends Canvas {
             Logger.getLogger(Framework.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /**
      * In specific intervals of time (GAME_UPDATE_PERIOD) the game/logic is updated and then the game is drawn on the screen.
      */
@@ -124,35 +126,35 @@ public class Framework extends Canvas {
     {
         // This two variables are used in VISUALIZING state of the game. We used them to wait some time so that we get correct frame/window resolution.
         long visualizingTime = 0, lastVisualizingTime = System.nanoTime();
-        
+
         // This variables are used for calculating the time that defines for how long we should put threat to sleep to meet the GAME_FPS.
         long beginTime, timeTaken, timeLeft;
-        
+
         while(true)
         {
             beginTime = System.nanoTime();
-            
+            sight.update();
             switch (gameState)
             {
                 case PLAYING:
                     gameTime += System.nanoTime() - lastTime;
-                    
+
                     game.UpdateGame(gameTime, mousePosition());
-                    
+
                     lastTime = System.nanoTime();
-                break;
+                    break;
                 case GAMEOVER:
                     //...
-                break;
+                    break;
                 case MAIN_MENU:
                     //...
-                break;
+                    break;
                 case OPTIONS:
                     //...
-                break;
+                    break;
                 case GAME_CONTENT_LOADING:
                     //...
-                break;
+                    break;
                 case STARTING:
                     // Sets variables and objects.
                     Initialize();
@@ -161,9 +163,9 @@ public class Framework extends Canvas {
 
                     // When all things that are called above finished, we change game status to main menu.
                     gameState = GameState.MAIN_MENU;
-                break;
+                    break;
                 case VISUALIZING:
-                    // On Ubuntu OS (when I tested on my old computer) this.getWidth() method doesn't return the correct value immediately (eg. for frame that should be 800px width, returns 0 than 790 and at last 798px). 
+                    // On Ubuntu OS (when I tested on my old computer) this.getWidth() method doesn't return the correct value immediately (eg. for frame that should be 800px width, returns 0 than 790 and at last 798px).
                     // So we wait one second for the window/frame to be set to its correct size. Just in case we
                     // also insert 'this.getWidth() > 1' condition in case when the window/frame size wasn't set in time,
                     // so that we although get approximately size.
@@ -180,25 +182,25 @@ public class Framework extends Canvas {
                         visualizingTime += System.nanoTime() - lastVisualizingTime;
                         lastVisualizingTime = System.nanoTime();
                     }
-                break;
+                    break;
             }
-            
+
             // Repaint the screen.
             repaint();
-            
+
             // Here we calculate the time that defines for how long we should put threat to sleep to meet the GAME_FPS.
             timeTaken = System.nanoTime() - beginTime;
             timeLeft = (GAME_UPDATE_PERIOD - timeTaken) / milisecInNanosec; // In milliseconds
             // If the time is less than 10 milliseconds, then we will put thread to sleep for 10 millisecond so that some other thread can do some work.
-            if (timeLeft < 10) 
+            if (timeLeft < 10)
                 timeLeft = 10; //set a minimum
             try {
-                 //Provides the necessary delay and also yields control so that other thread can do work.
-                 Thread.sleep(timeLeft);
+                //Provides the necessary delay and also yields control so that other thread can do work.
+                Thread.sleep(timeLeft);
             } catch (InterruptedException ex) { }
         }
     }
-    
+
     /**
      * Draw the game to the screen. It is called through repaint() method in GameLoop() method.
      */
@@ -209,14 +211,14 @@ public class Framework extends Canvas {
         {
             case PLAYING:
                 game.Draw(g2d, mousePosition());
-            break;
+                break;
             case GAMEOVER:
                 game.DrawGameOver(g2d, mousePosition());
-            break;
+                break;
             case MAIN_MENU:
                 g2d.drawImage(shootTheDuckMenuImg, 0, 0, frameWidth, frameHeight, null);
-                g2d.drawString("Use left mouse button to shot the duck.", frameWidth / 2 - 83, (int)(frameHeight * 0.65));   
-                g2d.drawString("Click with left mouse button to start the game.", frameWidth / 2 - 100, (int)(frameHeight * 0.67));                
+                g2d.drawString("Use left mouse button to shot the duck.", frameWidth / 2 - 83, (int)(frameHeight * 0.65));
+                g2d.drawString("Click with left mouse button to start the game.", frameWidth / 2 - 100, (int)(frameHeight * 0.67));
                 g2d.drawString("Press ESC any time to exit the game.", frameWidth / 2 - 75, (int)(frameHeight * 0.70));
                 g2d.setColor(Color.white);
                 g2d.drawString("WWW.GAMETUTORIAL.NET", 7, frameHeight - 5);
@@ -227,14 +229,17 @@ public class Framework extends Canvas {
                 break;
             case OPTIONS:
                 //...
-            break;
+                break;
             case GAME_CONTENT_LOADING:
                 g2d.setColor(Color.white);
                 g2d.drawString("GAME is LOADING", frameWidth / 2 - 50, frameHeight / 2);
-            break;
+                break;
         }
+        // Sight 이미지 그리기
+        sight.draw(g2d);
+
     }
-    
+
     /**
      * Starts new game.
      */
@@ -243,10 +248,10 @@ public class Framework extends Canvas {
         // We set gameTime to zero and lastTime to current time for later calculations.
         gameTime = 0;
         lastTime = System.nanoTime();
-        
+
         game = new Game();
     }
-    
+
     /**
      *  Restart game - reset game time and call RestartGame() method of game object so that reset some variables.
      */
@@ -255,17 +260,17 @@ public class Framework extends Canvas {
         // We set gameTime to zero and lastTime to current time for later calculations.
         gameTime = 0;
         lastTime = System.nanoTime();
-        
+
         game.RestartGame();
-        
+
         // We change game status so that the game can start.
         gameState = GameState.PLAYING;
     }
-    
+
     /**
      * Returns the position of the mouse pointer in game frame/window.
      * If mouse position is null than this method return 0,0 coordinate.
-     * 
+     *
      * @return Point of mouse coordinates.
      */
     private Point mousePosition()
@@ -273,7 +278,7 @@ public class Framework extends Canvas {
         try
         {
             Point mp = this.getMousePosition();
-            
+
             if(mp != null)
                 return this.getMousePosition();
             else
@@ -284,10 +289,10 @@ public class Framework extends Canvas {
             return new Point(0, 0);
         }
     }
-    
+
     /**
      * This method is called when keyboard key is released.
-     * 
+     *
      * @param e KeyEvent
      */
     @Override
@@ -300,29 +305,32 @@ public class Framework extends Canvas {
                     System.exit(0);
                 else if(e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_ENTER)
                     restartGame();
-            break;
+                break;
             case PLAYING:
             case MAIN_MENU:
                 if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
                     System.exit(0);
-            break;
+                break;
         }
     }
-    
+
     /**
      * This method is called when mouse button is clicked.
-     * 
+     *
      * @param e MouseEvent
      */
     @Override
-    public void mouseClicked(MouseEvent e)
-    {
-        switch (gameState)
-        {
+    public void mouseClicked(MouseEvent e) {
+        switch (gameState) {
             case MAIN_MENU:
-                if(e.getButton() == MouseEvent.BUTTON1)
+                if (e.getButton() == MouseEvent.BUTTON1) {
                     newGame();
-            break;
+                }
+                break;
+            case PLAYING:
+                // 클릭 시 Sight 크기 증가 시작
+                sight.startScaling(e.getX(), e.getY());
+                break;
         }
     }
     // 최고 점수를 얻는 메서드를 추가합니다.
---
+++ shoottheduck/src/main/java/kr/jbnu/se/std/Sight.java
@@ -0,0 +1,66 @@
+package kr.jbnu.se.std;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class Sight {
+    private BufferedImage sightImage;
+    private int x, y;
+    private float scale;
+    private boolean isScaling;
+    private long startTime;
+
+    public Sight() {
+        loadImage();
+        this.scale = 1.0f; // 초기 크기 비율
+        this.isScaling = false;
+    }
+
+    private void loadImage() {
+        try {
+            URL sightImgUrl = this.getClass().getResource("/images/sight.png");
+            sightImage = ImageIO.read(sightImgUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2d) {
+        // isScaling이 true일 때만 이미지를 그립니다.
+        if (isScaling && sightImage != null) {
+            int width = (int) (sightImage.getWidth() * scale);
+            int height = (int) (sightImage.getHeight() * scale);
+            g2d.drawImage(sightImage, x - width / 2, y - height / 2, width, height, null);
+        }
+    }
+
+    public void startScaling(int mouseX, int mouseY) {
+        this.x = mouseX;
+        this.y = mouseY;
+        this.scale = 1.0f; // 크기를 초기화
+        this.isScaling = true;
+        this.startTime = System.nanoTime();
+    }
+
+    public void update() {
+        if (isScaling) {
+            // 크기를 증가시키고, 0.35초가 지나면 스케일을 원래 크기로 줄입니다.
+            long elapsed = System.nanoTime() - startTime;
+            if (elapsed < 350_000_000L) { // 0.35초
+                scale = 1.5f; // 원하는 크기로 변경 (1.5배 예시)
+            } else {
+                scale = 1.0f; // 원래 크기로 되돌림
+                isScaling = false; // 스케일링 종료
+            }
+        }
+    }
+
+    public boolean isScaling() {
+        return isScaling;
+    }
+}
+
+
